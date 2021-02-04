fd2446e247d6593c6cad5438f911dd7934ff7e6a
kalyssao <kalyssaowusu@gmail.com>
2021-02-04 19:06:39 +0000

    Included future tests

---
+++ test/RegisterTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import org.junit.Assert;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RegisterTest {
+    @Test
+    public void testGetRegister(){
+
+    }
+}
