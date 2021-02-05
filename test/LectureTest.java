1f13f6ecbb93842e85f5dda8a57c2fa4160df904
kalyssao <kalyssaowusu@gmail.com>
2021-02-05 11:35:03 +0000

    Included Lecture & Register tests

--- test/LectureTest.java
+++ test/LectureTest.java
@@ -1,2 +1,31 @@
+import org.junit.Test;
+import org.junit.Assert;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LectureTest {
+    @Test
+    public void testGetHighestAverageGrade(){
+
+    }
+    @Test
+    public void testNaughtyStudentInLecture(){
+        List<Student> studentList = new ArrayList<>();
+        List<Double> gradeList = new ArrayList<>();
+        gradeList.add(85.0);
+        gradeList.add(70.0);
+        gradeList.add(65.0);
+
+        studentList.add(new Student("Bob", gradeList));
+        studentList.add(new Student("Bobbie", gradeList));
+
+        studentList.add(new NaughtyStudent("Jen", gradeList));
+        studentList.add(new NaughtyStudent("Joe", gradeList));
+
+        Lecture TLC = new Lecture(studentList);
+//        double highestAverageGrade = TLC.getHighestAverageGrade();
+//        two options -> compare expected w what we get vs.
+//        take naughty student & student avg grade, assert that highest avg grade equals naughty student & not regular student
+    }
+
 }
--- test/RegisterTest.java
+++ test/RegisterTest.java
@@ -1,11 +1,24 @@
 import org.junit.Test;
 import org.junit.Assert;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class RegisterTest {
     @Test
     public void testGetRegister(){
+        List<Nameable> tlc3 = new ArrayList<>();
+        List<String> expected = Arrays.asList("Bob", "Bob1", "Bob2", "Bob3");
 
+        tlc3.add(new Student("Bob"));
+        tlc3.add(new Student("Bob1"));
+        tlc3.add(new Student("Bob2"));
+        tlc3.add(new Student("Bob3"));
+
+        Register register = new Register(tlc3);
+        List<String> nameList = register.getRegister();
+
+        //Assert.assertArrayEquals(expected, nameList);
+        Assert.assertEquals(expected, nameList);
     }
 }
