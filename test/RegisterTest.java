e225b5499a99d7947ecdb23535d599cafda95a36
kalyssao <kalyssaowusu@gmail.com>
2021-02-07 21:06:32 +0000

    tested that exception is thrown, getStudentByName, and added setUp function

--- test/RegisterTest.java
+++ test/RegisterTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.Assert;
 import java.util.ArrayList;
@@ -5,6 +6,14 @@ import java.util.Arrays;
 import java.util.List;
 
 public class RegisterTest {
+
+    // need to figure out setup of "global" variables so
+    // we don't have to instantiate within every test
+    @Before
+    public void setUp(){
+
+    }
+
     @Test
     public void testGetRegister(){
         List<Student> tlc3 = new ArrayList<>();
@@ -24,6 +33,34 @@ public class RegisterTest {
 
     @Test
     public void testGetStudentByName(){
+        List<Student> tlc3 = new ArrayList<>();
+
+        tlc3.add(new Student("Bob"));
+        Register register = new Register(tlc3);
+
+        try {
+            Student studentOne = register.getStudentByName("Bob");
+            Assert.assertEquals("Bob", studentOne.getName());
+        }
+        catch (StudentNotFoundException exception){
+            // figure out best practice for catching
+            System.out.println("whoops!");
+        }
+
+    }
+
+    // Test expects a StudentNotFoundException to be thrown when
+    // the name of student is not in the register
+    @Test(expected = StudentNotFoundException.class)
+    public void testThrowsStudentNotFoundException()
+    throws StudentNotFoundException
+    {
+        List<Student> tlc3 = new ArrayList<>();
+
+        tlc3.add(new Student("Bob"));
+        Register register = new Register(tlc3);
+
+        Student studentOne = register.getStudentByName("Bob");
 
     }
 }
